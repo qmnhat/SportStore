98896d545ed128031fccbd3edce59a7c941384cb
Minh Nhật <0306231318@caothang.edu.vn>
2026-01-06 10:19:19 +0700

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,61 @@
+<div class="container-fluid px-5 d-none border-bottom d-lg-block">
+    <div class="row gx-0 align-items-center">
+        <div class="col-lg-4 text-center text-lg-start mb-lg-0">
+            <div class="d-inline-flex align-items-center" style="height: 45px;">
+            </div>
+        </div>
+        <div class="col-lg-4 text-center d-flex align-items-center justify-content-center">
+        </div>
+
+        <div class="col-lg-4 text-center text-lg-end">
+            <div class="d-inline-flex align-items-center" style="height: 45px;">
+                <div class="dropdown">
+                    <a href="#" class="dropdown-toggle text-muted ms-2" data-bs-toggle="dropdown"><small><i
+                                class="fa fa-home me-2"></i> My Dashboard</small></a>
+                    <div class="dropdown-menu rounded">
+                        <a href="#" class="dropdown-item"> Login</a>
+                        <a href="#" class="dropdown-item"> My Account</a>
+                        <a href="#" class="dropdown-item"> Log Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid px-5 py-4 d-none d-lg-block">
+    <div class="row gx-0 align-items-center text-center">
+        <div class="col-md-4 col-lg-3 text-center text-lg-start">
+            <div class="d-inline-flex align-items-center">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="display-5 text-primary m-0"><i
+                            class="fas fa-shopping-bag text-secondary me-2"></i>Electro</h1>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-6 text-center">
+            <div class="position-relative ps-4">
+                <div class="d-flex border rounded-pill">
+                    <input class="form-control border-0 rounded-pill w-100 py-3" type="text"
+                        placeholder="Search Looking For?">
+                    <select class="form-select text-dark border-0 border-start rounded-0 p-3" style="width: 200px;">
+                        <option value="All Category">All Category</option>
+                        <option value="Pest Control-2">Category 1</option>
+                    </select>
+                    <button type="button" class="btn btn-primary rounded-pill py-3 px-5" style="border: 0;"><i
+                            class="fas fa-search"></i></button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-lg-3 text-center text-lg-end">
+            <div class="d-inline-flex align-items-center">
+                <a href="#" class="text-muted d-flex align-items-center justify-content-center me-3"><span
+                        class="rounded-circle btn-md-square border"><i class="fas fa-random"></i></i></a>
+                <a href="#" class="text-muted d-flex align-items-center justify-content-center me-3"><span
+                        class="rounded-circle btn-md-square border"><i class="fas fa-heart"></i></a>
+                <a href="#" class="text-muted d-flex align-items-center justify-content-center"><span
+                        class="rounded-circle btn-md-square border"><i class="fas fa-shopping-cart"></i></span>
+                    <span class="text-dark ms-2">$0.00</span></a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,45 @@
+<div class="container-fluid nav-bar p-0">
+    <div class="row gx-0 bg-primary px-5 align-items-center">
+        <div class="col-lg-3 d-none d-lg-block">
+            <nav class="navbar navbar-light position-relative" style="width: 250px;">
+                <button class="navbar-toggler border-0 fs-4 w-100 px-0 text-start" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#allCat">
+                    <h4 class="m-0"><i class="fa fa-bars me-2"></i>All Categories</h4>
+                </button>
+                <div class="collapse navbar-collapse rounded-bottom" id="allCat">
+                    <div class="navbar-nav ms-auto py-0">
+                        <ul class="list-unstyled categories-bars">
+                            <li>
+                                <div class="categories-bars-item">
+                                    <a href="#">Accessories</a>
+                                    <span>(3)</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="col-12 col-lg-9">
+            <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
+                <a href="" class="navbar-brand d-block d-lg-none">
+                    <h1 class="display-5 text-secondary m-0"><i class="fas fa-shopping-bag text-white me-2"></i>Electro
+                    </h1>
+                </a>
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars fa-1x"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <a href="shop.html" class="nav-item nav-link">Shop</a>
+                        <a href="contact.html" class="nav-item nav-link me-2">Contact</a>
+                    </div>
+                    <a href="" class="btn btn-secondary rounded-pill py-2 px-4 px-lg-3 mb-3 mb-md-3 mb-lg-0"><i
+                            class="fa fa-mobile-alt me-2"></i> +0123 456 7890</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
