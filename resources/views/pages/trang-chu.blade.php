3906c61db218bd98312641e41d712ccfce8b3b1d
Minh Nhật <0306231318@caothang.edu.vn>
2026-01-08 09:02:41 +0700

    MinhNhat update trang chu

--- resources/views/pages/trang-chu.blade.php
+++ resources/views/pages/trang-chu.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid carousel bg-light px-0">
         <div class="row g-0 justify-content-end">
             <div class="col-12 col-lg-7 col-xl-9">
-                <div class="header-carousel owl-carousel bg-light py-5">
+                <div class="header-carousel owl-carousel bg-light ">
                     <div class="row g-0 header-carousel-item align-items-center">
                         <div class="col-xl-6 carousel-img wow fadeInLeft" data-wow-delay="0.1s">
                             <img src="img/ao/ao_3.jpg" class="img-fluid w-100" alt="Image">
@@ -38,7 +38,7 @@
             </div>
             <div class="col-12 col-lg-5 col-xl-3 wow fadeInRight" data-wow-delay="0.1s">
                 <div class="carousel-header-banner h-100">
-                    <img src="img/header-im g.jpg" class="img-fluid w-100 h-100" style="object-fit: cover;" alt="Image">
+                    <img src="img/nghia.jpg" class="img-fluid w-100 h-100" style="object-fit: cover;" alt="Image">
                     <div class="carousel-banner-offer">
                         <p class="bg-primary text-white rounded fs-5 py-2 px-4 mb-0 me-3">Save $48.00</p>
                         <p class="text-primary fs-5 fw-bold mb-0">Special Offer</p>
